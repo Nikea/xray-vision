be5b10b84868ae54654102129775c65d934dea14
Sameera Abeykoon <sameera@bnl.gov>
2015-07-19 15:42:44 -0400

    ENH: added more plotting tools for XSVS analysis, image, circular_average, roi_pixel_plotter

--- xray_vision/xsvs_plotting.py
+++ xray_vision/xsvs_plotting.py
@@ -47,7 +47,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-    Plotting tools for X-Ray Speckle Visibility Spectroscopy
+    Plotting tools for X-Ray Speckle Visibility Spectroscopy(XSVS)
 """
 
 
@@ -60,13 +60,15 @@ def mean_intensity_plotter(ax, mean_intensity_sets, index_list,
 
     Parameters
     ----------
-    ax : Axes
-
-    num_rois : int
+     ax : Axes
+        The `Axes` object to add the artist tool
 
     mean_intensity_sets : list
+        Average intensities of each ROI as a list
+        shape len(images_sets)
 
     index_list : list
+        labels list for each image set
 
     title : str, optional
         title of the plot
@@ -86,31 +88,30 @@ def mean_intensity_plotter(ax, mean_intensity_sets, index_list,
             total = 0
             first = 0
             x = [len(elm) for elm in mean_intensity_sets]
-            x_val=np.arange(sum(x))
+            x_val = np.arange(sum(x))
             for j in range(len(mean_intensity_sets)):
                 total += x[j]
                 ax[i].plot(x_val[first:total], mean_intensity_sets[j][:, i],
-                         label=str(j+1)+" image_set")
+                           label=str(j+1)+" image_set")
                 first = total
         plt.legend()
     else:
         raise ValueError("Labels list for the image sets are different")
 
 
-def combine_intensity_plotter(ax, combine_intensity, title="Mean intensities",
-                           xlabel="Frames", ylabel="Mean Intensity"):
+def combine_intensity_plotter(ax, combine_intensity,
+                              title="Mean Intensities - All Image Sets",
+                              xlabel="Frames", ylabel="Mean Intensity"):
     """
     This will plot the combine intensities for all image sets
 
     Parameters
     ----------
     ax : Axes
+        The `Axes` object to add the artist tool
 
-    num_rois : int
-
-    mean_intensity_sets : list
-
-    index_list : list
+    combine_intensity : array
+        combine intensities for each ROI from image data sets
 
     title : str, optional
         title of the plot
@@ -121,7 +122,8 @@ def combine_intensity_plotter(ax, combine_intensity, title="Mean intensities",
     y_label : str, optional
         y axis label
     """
-    for i in range(combin_intensity.shape[1]):
+    num_rois = combine_intensity.shape[1]
+    for i in range(num_rois):
         ax.set_ylabel(ylabel)
         ax_set_xlabel(xlabel)
         ax.set_title(title)
@@ -129,14 +131,53 @@ def combine_intensity_plotter(ax, combine_intensity, title="Mean intensities",
     plt.legend()
 
 
-def cirular_average_plotter(ax1, ax2, image_data, ring_averages, bin_centers,
-                            i_title="Image Data", c_title="Circular Average",
-                            cmap="gray", vmin=None, vmax=None, marker='o',
-                            line_color='blue', xlabel="Bin Centers",
-                            ylabel="Ring Average", left=0.1, width = 0.65,
-                            bottom= 0.1, height=0.65):
-    plot_axes = [left, bottom, width, height]
+def circular_average_plotter(ax1, ax2, image_data, ring_averages, bin_centers,
+                             i_title="Image Data", c_title="Circular Average",
+                             cmap="gray", vmin=None, vmax=None, marker='o',
+                             line_color='blue', xlabel="Bin Centers",
+                             ylabel="Ring Average"):
+    """
+    Parameters
+    ----------
+    ax1 : Axes
+        The `Axes` object to add the artist tool
 
+    ax2 : Axes
+        The `Axes` object to add the artist tool
+
+    image_data : array
+
+    ring_averages : array
+
+    bin_centers: array
+
+    i_title : str, optional
+        title for the image data
+
+    c_title : str, optional
+        title for the circular avergae of image data
+
+    cmap : str, optional
+        colormap for image data
+
+    vmin : float, optional
+        arguments specify the color limits
+
+    vmax : float, optional
+        arguments specify the color limits
+
+    marker : str, optional
+        line marker
+
+    line_color : str, optional
+        line color
+
+    x_label : str, optional
+        x axis label circular average plot
+
+    y_label : str, optional
+        y axis label circular average plot
+    """
     ax1.imshow(imge_data, cmap=cmap, vmin=vmin, vmax=vmax)
     ax1.set_title(i_title)
 
@@ -144,3 +185,69 @@ def cirular_average_plotter(ax1, ax2, image_data, ring_averages, bin_centers,
     ax2.set_title(c_title)
     ax2.set_xlabel(xlabel)
     ax2.set_ylabel(ylabel)
+
+
+def roi_kymograph_plotter(ax, kymograph_data, title="ROI Kymograph",
+                          fig_size=(8, 10), xlabel="pixel list",
+                          ylabel="Frames", cmap='gist_earth'):
+    """
+    This will plot graphical representation of pixels variation over time
+    for required ROI.
+
+    Parameters
+    ----------
+    ax : Axes
+        The `Axes` object to add the artist tool
+
+    kymograph_data : array
+        data for graphical representation of pixels variation over time
+        for required ROI
+
+    title : str, optional
+        title of the plot
+
+    fig_size : tuple, optional
+        figure size
+
+    x_label : str, optional
+        x axis label of the plot
+
+    y_label : str, optional
+        y axis label
+
+    cmap : str, optional
+        colormap for the data
+
+    """
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.imshow(kymograph_data, cmap=cmap)
+
+
+def roi_pixel_plotter(axes, roi_pixel_data, title='', xlabel='pixel list',
+                      ylabel='Intensity'):
+    """
+    ROI pixel plotter
+    Parameters
+    ----------
+    ax :
+
+    mean_intensity : array
+
+    title : str, optional
+
+    fig_size : tuple, optional
+
+    x_label : str, optional
+
+    y_label : str, optional
+
+    """
+    num_rois = roi_pixel_data.shape[1]
+    for i in range(num_rois):
+        axes[i].plot(roi_pixel_data[:, i])
+        axes[i].set_xlabel(xlabel)
+        axes[i].set_ylabel(ylabel)
+        axes[i].set_title(title)
+    plt.legend()
