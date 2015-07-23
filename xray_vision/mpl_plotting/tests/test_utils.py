3478b1f4b09f2c4218ea5e6fb96aca3a7a1ab6a3
Eric Dill <edill@bnl.gov>
2015-07-23 10:32:00 -0400

    ENH: Implement multi-line plotter for the roi_pixel_plotter

---
+++ xray_vision/mpl_plotting/tests/test_utils.py
@@ -0,0 +1,27 @@
+from xray_vision.mpl_plotting import utils
+from nose.tools import raises
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+@raises(ValueError)
+def _multiline_fail(ax, data, labels, line_kw=None, xlabels=None, 
+                    ylabels=None):
+    utils.multiline(ax, data, labels, line_kw, xlabels, ylabels)
+
+
+def _multiline_pass(ax, data, labels, line_kw=None, xlabels=None, 
+                    ylabels=None):
+    utils.multiline(ax, data, labels, line_kw, xlabels, ylabels)
+
+def test_multiline():
+    fig, ax = plt.subplots()
+    
+    fails = [
+        {'data': np.random.random((50, 3)), 'labels': '50x3'},
+        {'data': np.random.random((3, 50)), 'labels': '3x50'},
+        {'data': np.random.random((4, 4, 4)), 'labels': '4x4x4'},
+    ]
+    
+    for f in fails:
+        yield _multiline_fail, [ax], [f['data']], [f['labels']]
